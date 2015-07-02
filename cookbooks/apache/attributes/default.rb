1fa4c47f9f5533c87f2c15706a2044d8a3c269d8
yc26df1 <diego.furtado@macys.com>
2015-07-02 12:39:02 -0300

    2 Apache configuration example

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,1 +1,5 @@
+
+default["apache"]["home_index"] = "index1.html"
 default["apache"]["index_file"] = "index1.html"
+default["apache"]["sites"]["clowns"] = {"port" => 80}
+default["apache"]["sites"]["bears"] = {"port" => 81}
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,15 +1,38 @@
 package 'httpd' do
-    action :install
+  action :install
 end
 
 service 'httpd' do
-    action [:start, :enable]
+  supports :status => true, :restart => true, :reload => true
+  action [:start, :enable]
 end
 
-cookbook_file '/var/www/html/index.html' do
-    #source 'index.html'
-    source node['apache']['index_file']
+
+execute 'mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.disabled' do
+  only_if do
+    File.exists?('/etc/http/conf.d/welcome.conf')
+  end
+end
+
+node['apache']['sites'].each do | site_name, site_data |
+  document_root =  "/var/www/#{site_name}"
+  
+  template "/etc/httpd/conf.d/#{site_name}.conf" do
+    source 'httpd.conf.erb'
     mode '0644'
+    variables(:document_root => document_root, :port => site_data["port"])
+    notifies :restart, 'service[httpd]'
+  end
+
+  directory document_root do
+    mode '0755'
+    recursive true
+  end
+
+  template "#{document_root}/index.html" do
+    source 'index.html.erb'
+    mode '0644'
+    variables(:site_name => site_name, :port => site_data['port'])
+  end
+
 end
-
-
